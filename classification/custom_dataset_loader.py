8727aec0869f808dcb0f3349afedf4c7c7887977
jbawsume <jbawsume@yahoo.com>
2023-07-01 22:54:35 +0800

    epoch reduced to 3 and simple encoder x resize after tensor 13

--- classification/custom_dataset_loader.py
+++ classification/custom_dataset_loader.py
@@ -27,7 +27,8 @@ class StateFarmDataset(Dataset):
             transform = torchvision.transforms.Compose([
                 torchvision.transforms.ToPILImage(),
                 torchvision.transforms.Resize(128),
-                torchvision.transforms.ToTensor()
+                torchvision.transforms.ToTensor(),
+                torchvision.transforms.Normalize(mean=data_norm_mean, std=data_norm_std)
             ])
             image = transform(image)
 
